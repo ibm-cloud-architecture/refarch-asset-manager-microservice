598964e8375867725f3402c26339e01f0e1caad5
Jerome Boyer <boyerje@us.ibm.com>
2018-10-24 16:23:29 -0700

    modify addAsset

--- scripts/addAsset.sh
+++ scripts/addAsset.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set p = $(echo $PWD | awk -v h="scripts" '$0 ~h')
+if [[ $PWD = */scripts ]]; then
+ cd ..
+fi
 if [ $# -eq 1 ]
 then
   filen=$1
 else
-  filen="pumpDAL01.json"
+  filen="scripts/pumpDAL01.json"
 fi
 curl -H "Content-Type: application-json" --request POST --data @$filen http://assetmgr.greencompute.ibmcase.com:32544/assetmanager/assets
